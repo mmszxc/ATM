999acbc940315679c251ec4395af9750f37af15f
unknown <dangjian@redorcasz.com>
2022-10-26 10:19:48 +0800

    Initial commit

---
+++ bin/tets.py
@@ -0,0 +1,20 @@
+import os
+import re
+import shutil
+
+target = 'C:\\三江\\作业\\'
+for first_dirname, send_dirname, file_name in os.walk(r'C:\三江\1-30'):
+
+    for line in file_name:
+
+        res = first_dirname.strip().split('\\')
+        # res[-2]  day27
+        day_dir = res[-2]
+
+        if "作业" in line:
+
+            # print(line)
+            aaa = day_dir+line
+            #
+            # shutil.copy(os.path.join(first_dirname,line),target)
+            # os.rename(os.path.join(first_dirname,line),os.path.join(target,aaa))
